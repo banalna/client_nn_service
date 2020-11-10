ca2b359807172792bc1892c5ffdc948e9182d4f7
banalna <judas.priest999@gmail.com>
2020-11-10 20:51:58 +0300

    edit index.html

--- run_app.py
+++ run_app.py
@@ -11,6 +11,6 @@ app = create_app()
 #     'Notification': Notification, 'Task': Task, 'Room':Room}
 
 if __name__ == "__main__":
-    # app.run()
-    app.run(host="0.0.0.0", port=8080)
+    app.run(debug=True, port=5001)
+    # app.run(host="0.0.0.0", port=8080)
 
